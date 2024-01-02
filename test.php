0f3975479429ffcf6908a9654336ac765512ed02
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-01-02 10:54:25 +0100

    héritage extend classUser to classAdmin

--- test.php
+++ test.php
@@ -2,28 +2,35 @@
  
 declare(strict_types=1);
  
-class Admin
+class User
 {
-    private const STATUS_ACTIVE = 'active';
-    private const STATUS_INACTIVE = 'inactive';
+    public const STATUS_ACTIVE = 'active';
+    public const STATUS_INACTIVE = 'inactive';
  
-    // Ajout d'un tableau de roles pour affiner les droits des administrateurs :)
-    public function __construct(public string $username, private array $roles = [], private string $status = self::STATUS_ACTIVE)
+    public function __construct(public string $username, public string $status = self::STATUS_ACTIVE)
     {
     }
- 
+
     public function setStatus(string $status): void
     {
         if (!in_array($status, [self::STATUS_ACTIVE, self::STATUS_INACTIVE])) {
             trigger_error(sprintf('Le status %s n\'est pas valide. Les status possibles sont : %s', $status, implode(', ', [self::STATUS_ACTIVE, self::STATUS_INACTIVE])), E_USER_ERROR);
         };
- 
+
         $this->status = $status;
     }
- 
+
     public function getStatus(): string
     {
         return $this->status;
+    }
+}
+ 
+class Admin extends User
+{
+    // Ajout d'un tableau de roles pour affiner les droits des administrateurs :)
+    public function __construct(public string $username, public array $roles = [], public string $status = self::STATUS_ACTIVE)
+    {
     }
  
     // Méthode d'ajout d'un rôle, puis on supprime les doublons avec array_filter.
@@ -32,24 +39,18 @@ class Admin
         $this->roles[] = $role;
         $this->roles = array_filter($this->roles);
     }
-
+ 
     // Méthode de renvoie des rôles, dans lequel on définit le rôle ADMIN par défaut.
     public function getRoles(): array
     {
         $roles = $this->roles;
         $roles[] = 'ADMIN';
-
+ 
         return $roles;
     }
- 
+
     public function setRoles(array $roles): void
     {
         $this->roles = $roles;
     }
 }
-
-$user1=new Admin('nab',['admin'],'active');
-$user1->setStatus('inactive');
-$user1->setRoles(['simplyUser']);
-$user1->username='jean';
-var_dump($user1);
